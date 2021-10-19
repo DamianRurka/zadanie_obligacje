0d034fb996e0c13bc3fc4026fdb07e5fb7255e82
Damian Rurka <gladek123x@gmail.com>
2021-10-19 19:22:25 +0200

    zapis

--- main.py
+++ main.py
@@ -1,77 +1,107 @@
 import sys
-
-klasa = []
-osoba = []
-
-while True:
-    wejscie = input()
-    if wejscie !="":
-        if wejscie == "koniec":
-            if osoba:
-                klasa.append(osoba)
-            break
-        elif wejscie in ["wychowawca", "nauczyciel", "uczen"]:
-            if not osoba:
-                osoba.append(wejscie)
-            else:
-                klasa.append(osoba)
-                osoba = [wejscie]
-        else:
-            osoba.append(wejscie)
-print(klasa)
-
+#
+# klasa = []
+# osoba = []
+#
+# while True:
+#     wejscie = input()
+#     if wejscie !="":
+#         if wejscie == "koniec":
+#             if osoba:
+#                 klasa.append(osoba)
+#             break
+#         elif wejscie in ["wychowawca", "nauczyciel", "uczen"]:
+#             if not osoba:
+#                 osoba.append(wejscie)
+#             else:
+#                 klasa.append(osoba)
+#                 osoba = [wejscie]
+#         else:
+#             osoba.append(wejscie)
+# print(klasa)
 
 class Wychowawca:
-    def __init__(self, name, klasy):
-        self.name = name
-        self.klasy = klasy
+    def __init__(self):
+        self.name = None
+        self.klasy = []
 
+    def wczytaj(self):
+        self.name = input()
+
+        while True:
+            klasa = input()
+            if not klasa:
+                break
+            self.klasy.append(klasa)
+
+    def wypisz(self):
+        print("wychowawca {}, {}".format(self.name, self.klasy))
 
 class Nauczyciel:
-    def __init__(self, name, przedmiot, klasy):
-        self.name = name
-        self.przedmiot = przedmiot
-        self.klasy = klasy
+    def __init__(self):
+        self.name = None
+        self.przedmiot = None
+        self.klasy = []
 
+    def wczytaj(self):
+        self.name = input()
+        self.przedmiot = input()
 
+        while True:
+            klasa = input()
+            if not klasa:
+                break
+            self.klasy.append(klasa)
+
+    def wypisz(self):
+        print("Nauczyciel {}, uczący {}, {}".format(self.name, self.przedmiot, self.klasy))
 class Uczen:
-    def __init__(self, name, klasa):
-        self.name = name
-        self.klasa = klasa
+    def __init__(self):
+        self.name = None
+        self.klasa = None
+
+    def wczytaj(self):
+        self.name = input()
+        self.klasa = input()
+
+    def wypisz(self):
+        print("uczen {}, klasa {}".format(self.name, self.klasa))
+
+
+klasy = {}
 uczniowie = []
 nauczyciele = []
 wychowawcy = []
+
 while True:
     wejscie = input()
     if wejscie == "koniec":
         break
     elif wejscie == "uczen":
-        imie_nazwisko = input()
-        klasa = input()
-        uczen = Uczen(imie_nazwisko, klasa)
+        uczen = Uczen()
+        uczen.wczytaj()
         uczniowie.append(uczen)
+        if uczen.klasa in klasy.keys():
+            klasy[uczen.klasa].append(uczen)
+        else:
+            klasy[uczen.klasa] = [uczen]
     elif wejscie == "wychowawca":
-        imie_nazwisko = input()
-        klasy = []
-        while True:
-            klasa = input()
-            if not klasa:
-                break
-            klasy.append(klasa)
-        wychowawca = wychowawca(imie_nazwisko, klasy)
+        wychowawca = Wychowawca()
+        wychowawca.wczytaj()
         wychowawcy.append(wychowawca)
     elif wejscie == "nauczyciel":
-        imie_nazwisko = input()
-        przedmiot = input()
-        klasy = []
-        while True:
-            klasa = input()
-            if not klasa:
-                break
-            klasy.append(klasa)
-        nauczyciel = Nauczyciel(imie_nazwisko, przedmiot, klasy)
+        nauczyciel = Nauczyciel()
+        nauczyciel.wczytaj()
         nauczyciele.append(nauczyciel)
     else:
         print("złe dane")
         break
 
+# akcja = sys.argv[1]
+#
+# if len(akcja) > 2:
+#     for osoba in wychowawcy + nauczyciele + uczniowie:
+#         if osoba.name == akcja:
+#             osoba.wypisz()
+
+print(klasy)
