a2710e80c5134e51dd8e41df655d85b391e3dcb3
Damian Rurka <gladek123x@gmail.com>
2021-11-08 14:20:04 +0100

    pobieranie danych ze stron

--- PRACA_NA_MATERIALACH/Lekcja_Pobieranie_danych_z_internetu.py
+++ PRACA_NA_MATERIALACH/Lekcja_Pobieranie_danych_z_internetu.py
@@ -1,4 +1,11 @@
 import requests
 
 url = 'https://futurecollars.com/pl/business-intelligence-kurs-online/'
-resp = requests.get()
+resp = requests.get(url).text
+
+start_elem = '<h3 class="mentor-name">'
+start_pointer = resp.find(start_elem,0) #</h3>
+end_elem = '<\h3>'
+end_pointer = resp.find(end_elem,start_pointer+len(start_elem))
+print(resp[start_pointer+len(start_elem):end_pointer])
+
