1f95d5356549a8bb8128d2e3138cf5d5574e8698
Damian Rurka <gladek123x@gmail.com>
2021-11-08 21:56:28 +0100

    analiza warsztatów2

--- PRACA_NA_MATERIALACH/warsztaty_analiza_zadan.py
+++ PRACA_NA_MATERIALACH/warsztaty_analiza_zadan.py
@@ -1,1 +1,15 @@
 #Analiza Venv, i pip, skrótowe operacje na dict - warsztaty
+import requests
+
+url = "https://community-open-weather-map.p.rapidapi.com/weather"
+
+querystring = {"q":"London,uk","lat":"0","lon":"0","callback":"test","id":"2172797","lang":"null","units":"imperial","mode":"xml"}
+
+headers = {
+    'x-rapidapi-host': "community-open-weather-map.p.rapidapi.com",
+    'x-rapidapi-key': "SIGN-UP-FOR-KEY"
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)
