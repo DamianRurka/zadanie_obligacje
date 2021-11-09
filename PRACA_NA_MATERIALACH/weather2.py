a8fede157af3e73d05b6cf6e87673ac20a5f1347
Damian Rurka <gladek123x@gmail.com>
2021-11-09 15:38:31 +0100

    zadanie weather.py

--- PRACA_NA_MATERIALACH/weather2.py
+++ PRACA_NA_MATERIALACH/weather2.py
@@ -1,3 +1,4 @@
+import pprint
 import requests
 import sys
 import datetime
@@ -12,27 +13,14 @@ class POGODA:
         self.dane = self.zapisz_dane_dict
 
 
-    def zapisz_dane_dict(self):
+    def zapisz_dane_dict(self,):
         wyslane = {'key': self.Klucz, "q": "Malbork,uk", "lat": "0", "lon": "0",
                    "callback": "test", "id": "2172797",
                    "lang": "null", "units": "imperial", "mode": "xml"}
         pobrane = requests.get(link, params=wyslane)
-        self.req = pobrane.json()
-        return self.req
-
-    def slownik_danych(self):
-        with open('listadanych.txt', 'a') as plik:
-            plik.write(self.req.text)
-            dict(plik)
-            return self.zapisz_dane_dict
-
-    def status_opadow(self):
-        pass
-
+        req = pobrane.json()
+        return req
 
 wejscie= POGODA(Klucz = sys.argv[1], data = sys.argv[2])
-# print(wejscie.text)
-# print(wejscie.slownik_danych())
-#
-print(wejscie.zapisz_dane_dict())
-# #python weather.py API KEY 2021-11-11
+pprint.pprint(wejscie)
+
