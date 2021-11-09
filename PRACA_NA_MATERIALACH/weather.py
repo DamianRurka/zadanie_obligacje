82fbf837ff0df7cc74cc7489f1ab04de5bce3e07
Damian Rurka <gladek123x@gmail.com>
2021-11-09 16:34:58 +0100

    zadanie weather.py

--- PRACA_NA_MATERIALACH/weather.py
+++ PRACA_NA_MATERIALACH/weather.py
@@ -1,15 +1,17 @@
 import requests
 import sys
 import datetime
-
+przykladowa_data_w_slowniku = '2021-11-13'
 link = "https://weatherapi-com.p.rapidapi.com/current.json"
 today = datetime.date.today()
-
+# jak porównać czy wpisana data jest taka sama jak daty znajdujace się w plik.txt
 class POGODA:
     def __init__(self,Klucz,data=str(today)):
         self.Klucz = Klucz
         self.data = data
-        self.dane = self.zapisz_dane_dict
+        self.zapiszdane = self.zapisz_dane_dict
+        self.slownikdane = self.slownik_danych
+
 
 
     def zapisz_dane_dict(self):
@@ -20,19 +22,32 @@ class POGODA:
         self.req = pobrane.json()
         return self.req
 
-    def slownik_danych(self):
-        with open('listadanych.txt', 'a') as plik:
+    def slownik_danych(self): #dopisywanie a nie nadpisywanie danych w plik.txt
+        with open('zapis_pogody.txt', 'a') as plik:
             plik.write(self.req.text)
             dict(plik)
             return self.zapisz_dane_dict
 
+
     def status_opadow(self):
-        pass
+        opady = "precip_mm" #<-key
+        opady = pass
+
+        #jesli data byla juz wpisana sprawdz w slowniku dict(plik) czy opady sa równe czy wieksze od 0
+        #if opady == 0 print('nie pada)
+        #elif opady > 0 print('bedzie padac')
+        #else: print("nie wiem")
+        #jeßli data jest pierwszy raz wpisana poiteruj po slowniku otrzymanych danych i wypisz wynik operacji
+        #lub dodaj dane do pliku.txt i poiteruj po wczytanych danych
+        #najlepiej zeby  kazdy otrzymany wynik byl slownikiem a jego kluczem byla by data
+
 
 
 wejscie= POGODA(Klucz = sys.argv[1], data = sys.argv[2])
 # print(wejscie.text)
 # print(wejscie.slownik_danych())
-#
-print(wejscie.zapisz_dane_dict())
+#print(wejscie.json)
+print(wejscie)
+# print(wejscie.zapisz_dane_dict())
+
 # #python weather.py API KEY 2021-11-11
--- PRACA_NA_MATERIALACH/weathertest.py
+++ PRACA_NA_MATERIALACH/weathertest.py
@@ -1,25 +1,22 @@
 import requests
 import sys
-import datetime
-today = datetime.date.today()
 url = "https://weatherapi-com.p.rapidapi.com/current.json"
 
-class Pogoda:
-    def __init__(self,Klucz,data=str(today)):
-        self.Klucz = Klucz
-        self.data = data
-        self.dane= self.zapisz_dane_dict()
-
-    def zapisz_dane_dict(self):
-        querystring = {'key': self.Klucz, "q": "Malbork,uk", "lat": "0", "lon": "0",
+querystring = { "q": "Malbork,uk", "lat": "0", "lon": "0",
                    "callback": "test", "id": "2172797",
                    "lang": "null", "units": "imperial", "mode": "xml"}
-        pobrane = requests.get(url, params=querystring)
-        p=pobrane.json()
-        return p
 
-apiurl = "weatherapi-com.p.rapidapi.com",
-key = "aeb21c175emsh53abf9d14a8c350p1fa3fejsn3b832d50a076"
 
-wejscie= Pogoda(Klucz = sys.argv[1], data = sys.argv[2])
-print(wejscie)
+headers = {
+    'x-rapidapi-host': "weatherapi-com.p.rapidapi.com",
+    'x-rapidapi-key': "aeb21c175emsh53abf9d14a8c350p1fa3fejsn3b832d50a076"
+    }
+
+# headers = {
+#     'x-rapidapi-host': "weatherapi-com.p.rapidapi.com",'{}'.format(a)}
+# headers[1] = sys.argv[1]
+# a = headers[1]
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+
+print(response.text)
