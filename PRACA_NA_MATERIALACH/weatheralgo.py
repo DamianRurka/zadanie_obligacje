29d2ef2786f185b4993e176979f61b073fd3ae7a
Damian Rurka <gladek123x@gmail.com>
2021-11-11 19:42:39 +0100

    drobne poprawki , jeszcze nie działa

--- PRACA_NA_MATERIALACH/weatheralgo.py
+++ PRACA_NA_MATERIALACH/weatheralgo.py
@@ -32,6 +32,7 @@ class POGODA:
 
     def sprawdzanie_daty_w_slowniku(self):
         for daty in self.slownik_data_opady:
+
             if self.data in self.slownik_data_opady:
                 self.odczyt_pogody_format()
                 self.zapis_slownika_w_pliku()
@@ -61,15 +62,23 @@ class POGODA:
         with open('zapis_pogody.json', 'r') as plik:
             json.load(plik)
 
-            if self.slownik_data_opady['<co tu wpisac>'] == 0.0:
+            if self.slownik_data_opady[self.data_z_api] == 0.0:
                 print('Nie bedzie padac')
-            elif self.slownik_data_opady['<co tu wpisac>'] > 0.0:
+            elif self.slownik_data_opady[self.data_z_api] > 0.0:
                 print('bedzie padac')
             else:
                 print('nie wiem')
 
 
-wejscie= POGODA(Klucz = sys.argv[1], data = sys.argv[2])
+    #def slownik_docelowy(self): # ten slownik będzie przyjmował jako klucz date a
+    # wartość jako wynik funkcji status opadów i odnosił się do funkcji zapis slownika w pliku
+    # obecna funkcja zapis slownika do pliku bedzie musiala byc edytowana pod nazwe nowego slownika
+    # dodaj funkcje która wyswietli dane z pliku gdy podana data sys.argv w terminalu będzie znajdowała się
+    # w słowniku, wypisz nie wiem jesli podana data z przeszlosci w sys argv nie znajdowala się w pliku.json
+
+
+
+wejscie = POGODA(Klucz = sys.argv[1], data = sys.argv[2])
 print(wejscie.wyslij_dane_pobierajace())
 
 #terminal: python weatheralgo.py "aeb21c175emsh53abf9d14a8c350p1fa3fejsn3b832d50a076" 2021-11-10
