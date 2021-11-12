399cf9cbad0cfea7b9d0438ccbeaaa6aa7ce7e19
Damian Rurka <gladek123x@gmail.com>
2021-11-12 18:52:51 +0100

    drobne poprawki tylko wypisuje niewiem

--- PRACA_NA_MATERIALACH/weatheralgo.py
+++ PRACA_NA_MATERIALACH/weatheralgo.py
@@ -23,65 +23,64 @@ class POGODA:
         pobrane = requests.get(link, params=wyslane, headers=self.headers)
         self.req = pobrane.json()
         self.opady = self.req['current']['precip_mm']
-        if self.opady == 0.0:
-            self.opady = 'Nie będzie padać'
-        else:
-            self.opady = 'będzie padać'
-
-
-
+        self.format_opadow()
         self.data_z_api = self.req['location']['localtime'].split(' ')[0]
         self.pustyslownik()
-        self.sprawdzanie_daty_w_slowniku()
+        self.wczytanie_slownika_z_pliku()
 
     def pustyslownik(self):
         self.slownik_data_opady = []
 
     def sprawdzanie_daty_w_slowniku(self):
-        for daty in self.slownik_data_opady:
-
-            if self.data in self.slownik_data_opady:
-                self.odczyt_pogody_format()
-                self.zapis_slownika_w_pliku()
-                self.status_opadow()
-            else:
-                self.dodawanie_do_slownika()
-                self.odczyt_pogody_format()
-                self.zapis_slownika_w_pliku()
-                self.status_opadow()
+        if self.data in self.slownik_data_opady:
+            print(self.slownik_data_opady)
+            self.zapis_slownika_w_pliku()
+        if self.data == today:
+            self.dodawanie_do_slownika()
+            self.zapis_slownika_w_pliku()
+            self.wczytanie_slownika_z_pliku()
+            self.sprawdzanie_daty_w_slowniku()
+        else:
+            print('nie wiem')
+    def format_opadow(self):
+        if self.opady == 0.0:
+            self.opady = 'Nie będzie padać'
+        else:
+            self.opady = 'będzie padać'
 
     def dodawanie_do_slownika(self):
         self.slownik_data_opady[self.data_z_api] = self.opady
 
+    def wczytanie_slownika_z_pliku(self):
+        with open('zapis_pogody.json', 'r') as plik:
+            self.slownik_data_opady = plik
+            self.sprawdzanie_daty_w_slowniku()
 
     def zapis_slownika_w_pliku(self):
         with open('zapis_pogody.json', 'w') as plik:
             json.dump(self.slownik_data_opady, plik)
 
-    def odczyt_pogody_format(self):
-        self.format = {self.data_z_api : self.status_opadow()}
-        with open('odczyt_pogody.json', 'w') as plik:
-            json.dump(self.format, plik)
-        with open('odczyt_pogody.json', 'r') as plik:
-            print(json.load(plik))
+    # def odczyt_slownika(self):
+    #     self.format = {self.data_z_api : self.status_opadow()}
+    #     with open('odczyt_pogody.json', 'w') as plik:
+    #         json.dump(self.format, plik)
+    #     with open('odczyt_pogody.json', 'r') as plik:
+    #         print(json.load(plik))
 
-    def status_opadow(self):
-        with open('zapis_pogody.json', 'r') as plik:
-            json.load(plik)
+    # def status_opadow(self):
+    #     with open('zapis_pogody.json', 'r') as plik:
+    #         json.load(plik)
+    #
+    #         if self.slownik_data_opady[self.data_z_api] == 0.0:
+    #             print('Nie bedzie padac')
+    #         elif self.slownik_data_opady[self.data_z_api] > 0.0:
+    #             print('bedzie padac')
+    #         else:
+    #             print('nie wiem')
 
-            if self.slownik_data_opady[self.data_z_api] == 0.0:
-                print('Nie bedzie padac')
-            elif self.slownik_data_opady[self.data_z_api] > 0.0:
-                print('bedzie padac')
-            else:
-                print('nie wiem')
 
-
-    #def slownik_docelowy(self): # ten slownik będzie przyjmował jako klucz date a
-    # wartość jako wynik funkcji status opadów i odnosił się do funkcji zapis slownika w pliku
-    # obecna funkcja zapis slownika do pliku bedzie musiala byc edytowana pod nazwe nowego slownika
     # dodaj funkcje która wyswietli dane z pliku gdy podana data sys.argv w terminalu będzie znajdowała się
-    # w słowniku, wypisz nie wiem jesli podana data z przeszlosci w sys argv nie znajdowala się w pliku.json lub
+    # w słowniku który zostanie wczytany z pliku.json, wypisz nie wiem jesli podana data z przeszlosci w sys argv nie znajdowala się w pliku.json lub
     # data jest z przyszłości
 
 
